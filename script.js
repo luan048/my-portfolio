fbac6d55a69743a39885e0d16b9a5d43e6823f6a
luan048 <luanmonteiropedrosa@gmail.com>
2024-12-24 00:12:48 -0300

    Update in the front-end

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.elementsMenu a, .voltar').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const targetId = this.getAttribute('href').replace('#', '')
+
+        if (targetId) {
+            const targetElement = document.getElementById(targetId)
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
+            }
+        } else {
+            // Rolar suavemente para o topo
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+        }
+    })
+})
